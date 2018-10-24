f3c39a4fb510932753edfecc69776a84c2a0aca5
Cristiano Gavião <cvgaviao@gmail.com>
2018-10-24 17:19:27 -0300

    1: Contributing the osgi-container-maven-plugin to open source
    Task-Url: https://github.com/cvgaviao/osgi-container-maven-plugin/issues/1

--- src/main/java/br/com/c8tech/tools/maven/plugin/osgi/container/MavenRegistryAuthSupplier.java
+++ src/main/java/br/com/c8tech/tools/maven/plugin/osgi/container/MavenRegistryAuthSupplier.java
@@ -1,3 +1,16 @@
+/**
+ * ==========================================================================
+ * Copyright © 2015-2018 Cristiano Gavião, C8 Technology ME.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Cristiano Gavião (cvgaviao@c8tech.com.br)- initial API and implementation
+ * ==========================================================================
+ */
 
 /*-
  * -\-\-
